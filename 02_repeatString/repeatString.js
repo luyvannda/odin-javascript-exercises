104e086ab45c7e34df895dab9ceabf265bc9ddf8
Luy Vannda <philips.joe329@gmail.com>
2023-07-17 15:55:51 +0700

    complete 02_repeatString exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
+const repeatString = function (string, num) {
+  let output = '';
 
-};
+  if (num < 0) {
+    return "ERROR";
+  } else {
+    for (let i = 1; i <= num; i++) {
+      output += string;
+    }
 
+    return output;
+  }
+}
+let result = repeatString('hey', 1);
+console.log(result);
 // Do not edit below this line
 module.exports = repeatString;
