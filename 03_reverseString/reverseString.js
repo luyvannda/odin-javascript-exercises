9c06c6f5b0b9be7dca37c396c44840d57052b7d9
Luy Vannda <philips.joe329@gmail.com>
2023-07-18 09:00:25 +0700

    Complete reverseString exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function (words) {
+  let splitWords = words.split("");
+  let output = "";
+  for (let i = splitWords.length - 1; i >= 0; i--) {
+    output += splitWords[i];
+  }
+  return output;
 };
+let result = reverseString('hello')
+console.log(result);
 
 // Do not edit below this line
 module.exports = reverseString;
