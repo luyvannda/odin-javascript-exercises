bae256f5636f39feb088acb1bef57d9f9f5ab03e
luyvannda <philips.joe329@gmail.com>
2023-07-18 15:38:34 +0700

    complete leapYears exercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,14 @@
-const leapYears = function() {
+const leapYears = function (years) {
+  let isLeapYears = false;
 
+  if (years % 100 === 0 && years % 400 !== 0) {
+    isLeapYears = false;
+  } else if (years % 4 === 0 || years % 400 === 0) {
+    isLeapYears = true;
+  }
+  return isLeapYears;
 };
-
+let result = leapYears(1900);
+console.log(result);
 // Do not edit below this line
 module.exports = leapYears;
