0f31d1e549ceb2c9eaf95e7e6c5db7d527b79247
luyvannda <philips.joe329@gmail.com>
2023-07-18 16:13:57 +0700

    complete tempConversion exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,16 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (Fahrenheit) {
+  let output = (Fahrenheit - 32) * 5 / 9
+  return parseFloat(output.toFixed(1));
 };
+let isCelsius = convertToCelsius(100)
+console.log(`Celsius temperature = ${isCelsius}`);
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (Celsius) {
+  let output = (Celsius * 1.8) + 32;
+  return parseFloat(output.toFixed(1));
 };
+let isFahrenheit = convertToFahrenheit(73.2);
+console.log(`Fahrenheit temperature = ${isFahrenheit}`);
 
 // Do not edit below this line
 module.exports = {
