a674bd06db532a03a5b3b655a8a2e67a284f12bc
luyvannda <philips.joe329@gmail.com>
2023-08-14 15:54:13 +0700

    complete function sum for all tests

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -6,8 +6,16 @@ const subtract = function (...args) {
   return args[0] - args[1];
 };
 
-const sum = function () {
-
+const sum = function (arr) {
+  let sum = 0;
+  if (arr === []) {
+    sum = 0
+  } else if (arr.length === 1) {
+    sum = arr[0];
+  } else {
+    sum = arr.reduce((sum, num) => sum + num, 0);
+  }
+  return sum;
 };
 
 const multiply = function () {
