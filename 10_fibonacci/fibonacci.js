289ac0351c9fff4efcc3cd58709c500244d2d3a3
luyvannda <philips.joe329@gmail.com>
2023-08-15 15:14:19 +0700

    pass 5 test with fibonacci function

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,13 @@
-const fibonacci = function() {
+const fibonacci = function (order) {
+  let fibs = [1, 1];
 
+  for (let i = 2; i < order; i++) {
+    fibs.push(fibs[fibs.length - 1] + fibs[fibs.length - 2]);
+  }
+  console.log(fibs);
+  return fibs[order - 1];
 };
-
+let result = fibonacci(6)
+console.log(result);
 // Do not edit below this line
 module.exports = fibonacci;
