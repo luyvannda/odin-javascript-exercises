7d0e308a9f949764756bfc27ccd9a44e6482b3a3
luyvannda <philips.joe329@gmail.com>
2023-08-15 14:29:04 +0700

    Complete palindromes exercises

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,16 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  let lowerCase = str.toLowerCase();
+  let splitStr = lowerCase.trim().split(/\W+/g);
+  let reverseStr = lowerCase.split(/\W+/g)
+    .join("")
+    .split("")
+    .reverse();
+  console.log(splitStr.join(""));
+  console.log(reverseStr.join(""));
+  return splitStr.join("") === reverseStr.join("")
 };
 
+let result = palindromes('A car, a man, a maraca.');
+console.log(result);
 // Do not edit below this line
 module.exports = palindromes;
