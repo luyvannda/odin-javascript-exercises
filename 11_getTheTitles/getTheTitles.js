e45be53a6a9f89dea88f03091815d9a595bde5e7
luyvannda <philips.joe329@gmail.com>
2023-08-15 15:37:21 +0700

    use Array.prototype.map to get the titles of every book in a given array object

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,21 @@
-const getTheTitles = function() {
 
+const getTheTitles = function (obj) {
+  return obj.map((book) => book.title);
 };
+
+const books = [
+  {
+    title: 'Book',
+    author: 'Name'
+  },
+  {
+    title: 'Book2',
+    author: 'Name2'
+  }
+]
+
+let result = getTheTitles(books);
+console.log(result);
 
 // Do not edit below this line
 module.exports = getTheTitles;
