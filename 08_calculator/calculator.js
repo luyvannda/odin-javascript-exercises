1da004237093de85b6daec7b45e4ed3b943ca297
luyvannda <philips.joe329@gmail.com>
2023-08-15 09:52:36 +0700

    complete the calculator exercise by using recursion to compute the factorial of any number

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -26,8 +26,14 @@ const power = function (...args) {
   return Math.pow(args[0], args[1]);
 };
 
-const factorial = function () {
-
+const factorial = function (num) {
+  if (num < 0) {
+    return -1;
+  } else if (num === 0 || num === 1) {
+    return 1;
+  } else {
+    return num * factorial(num - 1);
+  }
 };
 
 // Do not edit below this line
