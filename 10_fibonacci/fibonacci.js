c7b720a80bcf76b37a7e51d703996a514dc818e3
luyvannda <philips.joe329@gmail.com>
2023-08-15 15:23:01 +0700

    complete all tests for fibonnaci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,13 +1,17 @@
 const fibonacci = function (order) {
   let fibs = [1, 1];
-
+  if (order < 0) {
+    return "OOPS"
+  } else if (typeof order === 'string') {
+    +order;
+  }
   for (let i = 2; i < order; i++) {
     fibs.push(fibs[fibs.length - 1] + fibs[fibs.length - 2]);
   }
   console.log(fibs);
   return fibs[order - 1];
 };
-let result = fibonacci(6)
+let result = fibonacci('6')
 console.log(result);
 // Do not edit below this line
 module.exports = fibonacci;
