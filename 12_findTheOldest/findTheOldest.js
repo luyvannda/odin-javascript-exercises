0cb613859847ad532c4a0925f75c558c6e0e5041
luyvannda <philips.joe329@gmail.com>
2023-08-16 09:15:32 +0700

    Use reduce to find the oldest person in an array object

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,38 @@
-const findTheOldest = function() {
+const people = [
+  {
+    name: "Carly",
+    yearOfBirth: 2018,
+  },
+  {
+    name: "Ray",
+    yearOfBirth: 1962,
+    yearOfDeath: 2011,
+  },
+  {
+    name: "Jane",
+    yearOfBirth: 1912,
+    yearOfDeath: 1941,
+  },
+]
 
+
+const findTheOldest = function (arr) {
+
+  let currentYear = new Date().getFullYear();
+  console.log(currentYear);
+
+  return arr.reduce((next, last) => {
+    let nextYearOfDeath = next.yearOfDeath || currentYear;
+    let lastYearOfDeath = last.yearOfDeath || currentYear;
+
+    let nextGuy = nextYearOfDeath - next.yearOfBirth;
+    let lastGuy = lastYearOfDeath - last.yearOfBirth;
+
+    return nextGuy < lastGuy ? last : next;
+  })
 };
+let result = findTheOldest(people);
+console.log(result);
 
 // Do not edit below this line
 module.exports = findTheOldest;
